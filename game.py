a7499d9d8fac572f57f175da719e605bd8988bd9
kirillanpilov <anpilov.kirill2002@gmail.com>
2023-05-31 21:50:27 +0300

    Add ship class, the last one was deleted

--- game.py
+++ game.py
@@ -1,10 +1,23 @@
 import pygame
 from const import *
-
+from ship import *
 
 
 class Game:
+    def __init__(self):
+        self.gameOver = False
+        self.ship = Ship()
+        self.bg = pygame.image.load("assets/images/space_background.png")
+
+    def draw_screen(self, surface: pygame.surface):
+        surface.blit(self.bg, (0, 0))
+
+    def draw_ship(self, surface: pygame.surface):
+        surface.blit(self.ship.rotatedSurf, self.ship.rotatedRect)
+
+        if self.ship.isMoveForward:
+            surface.blit(self.ship.rotatedSurfEngine, self.ship.rotatedRect)
 
 
 
-    def draw_
+
